e93d3b7757d9c9f5dc8c085409d7889aeb041793
youssef faouzi <yj1984515@gmail.com>
2026-09-03 11:59:54 +0200

    Add list comprehension examples for squares, evens, and values greater than five in ch11.py

---
+++ jour1/LISTES/ch11.py
@@ -0,0 +1,9 @@
+
+nombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+carres = [x**2 for x in nombres]
+print(carres)
+paire = [x for x in nombres if x % 2 == 0]
+print(paire)
+listS5 = [x for x in nombres if x > 5]
+print(listS5)
