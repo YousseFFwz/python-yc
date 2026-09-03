c0bb4920f97ef247f8b00215429e7acc38cc1f4e
youssef faouzi <yj1984515@gmail.com>
2026-09-03 13:09:03 +0200

    Add student dictionary example with technology skills and updated level in ch6.py

---
+++ jour1/DICTIONNAIRES/ch6.py
@@ -0,0 +1,8 @@
+etudiant = {
+    "nom": "Omar", "age": 22,
+    "formation": {"nom": "Développement IA", "niveau": "Avancé", "duree": 12}
+}
+
+etudiant["formation"]["niveau"] = "expert"
+etudiant["technologies"] =  ["Python", "SQL", "Pandas", "MachineLearning"]
+print(etudiant)
